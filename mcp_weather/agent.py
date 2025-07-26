12456e19c1baafdd1189181032b7084aac054f1e
Mohamed Nady <mohamed0011199@gmail.com>
2025-07-26 20:25:34 +0000

    final

--- mcp_weather/agent.py
+++
@@ -1,6 +0,0 @@
-class Agent():
-    def __init__(self, name="Agent"):
-        self.name = name
-    def respond(self,context):
-        last= context[-1].content if context else ""
-        return f"Echo: '{last}'understood"
